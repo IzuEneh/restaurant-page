bdd421bb67fd6b4ff263602ce9199f9360ff7eb1
IzuEneh <izueneh21@gmail.com>
2022-12-14 20:05:21 -0700

    Implement contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,24 @@
-import "../styles/home.css";
+import "../styles/contact.css";
 import "../style.css";
+import loc from "../assets/pellegrino.png";
 
 export default function createContactPage() {
 	const container = document.createElement("div");
-	container.classList.add("home-container");
+	container.classList.add("contact-container");
 	container.classList.add("transparent-bg");
-	container.textContent = "Contact page";
+
+	const callText = document.createElement("p");
+	callText.textContent = "call: 123-345-6789";
+
+	const locationText = document.createElement("p");
+	locationText.textContent = "location: 24016 Province of Bergamo, Italy";
+
+	const locationImage = document.createElement("img");
+	locationImage.src = loc;
+	locationImage.classList.add("location-img");
+
+	container.appendChild(callText);
+	container.appendChild(locationText);
+	container.appendChild(locationImage);
 	return container;
 }
